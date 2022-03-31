237f0d49b745b1874e827ffb8e022c2e0029f994
xilverh0ya <xilver0203@naver.com>
2022-03-31 18:47:17 +0900

    Post Test

---
+++ _posts/python/2022-03-31-jumpToPython_00_Tutorial.md
@@ -0,0 +1,42 @@
+---
+title: "[2022) Jump to Python Study 00. Tutorial]"
+categories:
+  - python
+tags:
+  - minimal mistakes
+  - jekyll
+  - blog
+  - favicon
+  - disqus
+  - google analytics
+toc: true
+toc_sticky: true
+toc_label: "GITHUB BLOG JEKYLL"
+toc_icon: "bookmark"
+---
+
+<br>
+
+# 작성중
+
+작성예정입니다.  
+
+test  
+
+```bash
+git push origin master
+```
+
+```python
+print('helloWorld')
+```
+
+```java
+public static void main(String[] args){
+    System.out.println("Hello World!");
+}
+```
+
+___
+
+test page
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "[2022) Jump to Python Study 00. Tutorial]",
+        "excerpt":"    작성중   작성예정입니다.   test   git push origin master   print('helloWorld')   public static void main(String[] args){     System.out.println(\"Hello World!\"); }     test page  ","categories": ["python"],
+        "tags": ["minimal mistakes","jekyll","blog","favicon","disqus","google analytics"],
+        "url": "http://localhost:4000/python/jumpToPython_00_Tutorial/",
+        "teaser": null
+      }]
